00a8f23f70740a854fd9202facb2808460e17dc9
mikegit77 <mikegit77@hotmail.com>
2012-11-03 13:10:33 -0700

    add comment headers at top of JS files

--- singleClientBasicUI.js
+++ singleClientBasicUI.js
@@ -1,48 +1,57 @@
+//
+// This file contains javascript source for manipulating basic UI elements.
+//
+// See file "singleClientData.js" for source code to setup visualizations 
+// and populate data.
+//
+
+
+
 // set initial animation widths for the progress meters (under homework)
 $(function() {
-	$(".meter > span").each(function() {
-		$(this)
-			.data("origWidth", $(this).width())
-			.width(0)
-			.animate({
-				width: $(this).data("origWidth")
-			}, 1200);
-	});
+    $(".meter > span").each(function() {
+        $(this)
+            .data("origWidth", $(this).width())
+            .width(0)
+            .animate({
+                width: $(this).data("origWidth")
+            }, 1200);
+    });
 });
 
 // activate animations for tabs at top of page (today, show multiClientView)
 $(document).ready(function(){
-	$(".trigger").click(function(){
-		$(".panel").toggle("fast");
-		$(this).toggleClass("active");
-		return false;
-	});
+    $(".trigger").click(function(){
+        $(".panel").toggle("fast");
+        $(this).toggleClass("active");
+        return false;
+    });
 });
 
 // turn on/off visualization series when someone clicks on a checkbox
 function visControlChanged(e) {
     switch(e.target.id) {
     case "chkDepression":
-		e.target.checked ? window.chart.series[0].show() : window.chart.series[0].hide();
+        e.target.checked ? window.chart.series[0].show() : window.chart.series[0].hide();
         break;
         
     case "chkAnxiety":
-		e.target.checked ? window.chart.series[1].show() : window.chart.series[1].hide();
+        e.target.checked ? window.chart.series[1].show() : window.chart.series[1].hide();
         break;
         
     case "chkStress1":
-		e.target.checked ? window.chart.series[2].show() : window.chart.series[2].hide();
+        e.target.checked ? window.chart.series[2].show() : window.chart.series[2].hide();
         break;
 
     case "chkSuicide":
-   		e.target.checked ? window.chart.series[3].show() : window.chart.series[3].hide();
+           e.target.checked ? window.chart.series[3].show() : window.chart.series[3].hide();
         break;
 
     case "chkAAQ2":
         e.target.checked ? window.chart.series[4].show() : window.chart.series[4].hide();
         break;
 
-	case "chkTrendLine":
+    case "chkTrendLine":
         alert("checkbox: " + e.target.id + " is now " + (e.target.checked ? "checked" : "UNchecked"));
         break;
 
